fe7a58644e386b52bedf2d142dc1d315074685bf
ArKc1009 <arkc1009@gmail.com>
2021-03-08 00:03:17 +0900

    fix: import 위치 변경

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import App from './App.jsx';
 import {createGlobalStyle} from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Noto+Sans+KR:wght@100;300&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap');
+
   body {
-    @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Noto+Sans+KR:wght@100;300&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap');
     background-color: #e5e5e5;
+    font-family: 'Bebas Neue', cursive;
+    font-family: 'Noto Sans KR', sans-serif;
+    font-family: 'Nanum Gothic', sans-serif;
   }
 
   * {
