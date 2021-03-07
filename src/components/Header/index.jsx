13706ab1ece3e3dfe7a38416fda97bad096207ee
ArKc1009 <arkc1009@gmail.com>
2021-03-07 22:31:24 +0900

    chore: 필요없는 import 제거

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, {keyframes} from "styled-components";
+import styled from "styled-components";
 import FirstRender from "../../animations/RenderAnimate/FirstRender";
 
 const StyledHeader = styled.div`
