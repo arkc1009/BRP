3a2e871722eaa9b863a2156eb9d29ce3b70edb89
ArKc1009 <arkc1009@gmail.com>
2021-03-07 22:31:11 +0900

    feat: Animate 추가

---
+++ src/animations/RenderAnimate/OpacityAnimate.jsx
@@ -0,0 +1,12 @@
+import {keyframes} from "styled-components";
+
+const OpacityAnimate = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 100%;
+  }
+`;
+
+export default OpacityAnimate;
