617e39caddb8d0965bf387447e1a85345b9384c0
ArKc1009 <arkc1009@gmail.com>
2021-03-10 00:21:53 +0900

    fix: global Style을 불안정한 styled-component 형식에서 일반적인 방식으로 변경

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import {createGlobalStyle} from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Noto+Sans+KR:wght@100;300&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap');
-
-  body {
-    background-color: #e5e5e5;
-    font-family: 'Bebas Neue', cursive;
-    font-family: 'Noto Sans KR', sans-serif;
-    font-family: 'Nanum Gothic', sans-serif;
-  }
-
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-`;
+import '../src/style/global.css';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<App/>
-		<GlobalStyle/>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
