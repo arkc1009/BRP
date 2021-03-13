cb2717ed88c330b8971b0d11811769cd6840f5d2
ArKc1009 <arkc1009@gmail.com>
2021-03-13 23:50:43 +0900

    feat: route Pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import {Route, BrowserRouter} from "react-router-dom";
 import '../src/style/global.css';
+import App from './App.jsx';
+import FirstPage from "./FirstPage";
+
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App/>
+		<BrowserRouter>
+			<Route exact path='/' component={FirstPage}/>
+			<Route path='/main' component={App}/>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
